066a00a9caedc1addfb0156b4396fc2b42c47645
unknown <iccc97@gmail.com>
2022-08-14 23:30:08 +0300

    added styles FriendList

--- react-hw-components/src/components/FriendList/FriendList.jsx
+++ react-hw-components/src/components/FriendList/FriendList.jsx
@@ -4,16 +4,20 @@ import friends from "../../assets/friends.json";
 
 import { FriendListItem } from "./FriendListItem";
 
+import "./FriendList.css";
+
 export const FriendList = () => {
     return (
-        <ul className="friend-list">
-            {friends.map(({ id, avatar, name }) => (
-                <FriendListItem
-                    key={id}
-                    avatar={avatar}
-                    name={name}
-                />
-            ))}
-        </ul>
+        <section className="Friends">
+            <ul className="friend-list">
+                {friends.map(({ id, avatar, name }) => (
+                    <FriendListItem
+                        key={id}
+                        avatar={avatar}
+                        name={name}
+                    />
+                ))}
+            </ul>
+        </section>
     );
 };
--- react-hw-components/src/components/FriendList/FriendListItem.jsx
+++ react-hw-components/src/components/FriendList/FriendListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
     return (
-        <li className="item">
+        <li className="listItem">
             <span className={isOnline ? 'online' : 'offline'}></span>
             <img className="avatar" src={avatar} alt="User avatar" width="48" />
             <p className="name">{name}</p>
